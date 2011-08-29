b6bd029813ee1e8cd51e892387c834f2eaab4cda
asypost <asypost@gmail.com>
2011-08-29 06:40:00 +0000

    Add a log class to format log output,fix bugs.

--- socks5.py
+++ socks5.py
@@ -9,18 +9,44 @@ import signal
 SOCKTIMEOUT=5#客户端连接超时(秒)
 RESENDTIMEOUT=300#转发超时(秒)
 
-VER=chr(0x05)
-METHOD=chr(0x00)
+VER="\x05"
+METHOD="\x00"
 
-SUCCESS=chr(0x00)
-SOCKFAIL=chr(0x01)
-NETWORKFAIL=chr(0x02)
-HOSTFAIL=chr(0x04)
-REFUSED=chr(0x05)
-TTLEXPIRED=chr(0x06)
-UNSUPPORTCMD=chr(0x07)
-ADDRTYPEUNSPPORT=chr(0x08)
-UNASSIGNED=chr(0x09)
+SUCCESS="\x00"
+SOCKFAIL="\x01"
+NETWORKFAIL="\x02"
+HOSTFAIL="\x04"
+REFUSED="\x05"
+TTLEXPIRED="\x06"
+UNSUPPORTCMD="\x07"
+ADDRTYPEUNSPPORT="\x08"
+UNASSIGNED="\x09"
+
+_LOGGER=None
+
+class Log:
+	WARN="[WARN:]"
+	INFO="[INFO:]"
+	ERROR="[ERROR:]"
+	def write(self,message,level):
+		pass
+		
+class SimpleLog(Log):
+	import sys
+	def __init__(self,output=sys.stdout):
+		self.__output=output
+		self.show_log=True
+		
+	def write(self,message,level=Log.INFO):
+		if self.show_log:
+			self.__output.write("%s\t%s\n" %(level,message))
+			
+def getLogger(output=sys.stdout):
+	global _LOGGER
+	if not _LOGGER:
+		_LOGGER=SimpleLog(output)
+	return _LOGGER
+		
 
 class SocketTransform(Thread):
 	def __init__(self,src,dest_ip,dest_port,bind=False):
@@ -35,7 +61,7 @@ class SocketTransform(Thread):
 		try:
 			self.resend()
 		except Exception,e:
-			sys.stderr.write("Error on SocketTransform %s\n" %(e.message,))
+			getLogger().write("Error on SocketTransform %s" %(e.message,),Log.ERROR)
 			self.sock.close()
 			self.dest.close()
 
@@ -44,10 +70,10 @@ class SocketTransform(Thread):
 		self.dest=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		self.dest.connect((self.dest_ip,self.dest_port))
 		if self.bind:
-			sys.stdout.write("waiting for the client\n")
+			getLogger().write("Waiting for the client")
 			self.sock,info=sock.accept()
-			sys.stdout.write("Client connected\n")
-		sys.stdout.write("Starting Resending\n")
+			getLogger().write("Client connected")
+		getLogger().write("Starting Resending")
 		self.sock.settimeout(RESENDTIMEOUT)
 		self.dest.settimeout(RESENDTIMEOUT)
 		Resender(self.sock,self.dest).start()
@@ -65,7 +91,7 @@ class Resender(Thread):
 		try:
 			self.resend(self.src,self.dest)
 		except Exception,e:
-			sys.stderr.write("Connection lost %s\n" %(e.message,))
+			getLogger().write("Connection lost %s" %(e.message,),Log.ERROR)
 			self.src.close()
 			self.dest.close()
 
@@ -76,7 +102,7 @@ class Resender(Thread):
 			data=src.recv(10)
 		src.close()
 		dest.close()
-		sys.stdout.write("Client quit normally\n")
+		getLogger().write("Client quit normally\n")
 
 
 def create_server(ip,port):
@@ -87,46 +113,46 @@ def create_server(ip,port):
 	while True:
 		sock,addr_info=transformer.accept()
 		sock.settimeout(SOCKTIMEOUT)
-		sys.stdout.write("\nGot one client connection\n")
+		getLogger().write("Got one client connection")
 		try:
 			ver,nmethods,methods=(sock.recv(1),sock.recv(1),sock.recv(1))
 			sock.sendall(VER+METHOD)
 			ver,cmd,rsv,atyp=(sock.recv(1),sock.recv(1),sock.recv(1),sock.recv(1))
 			dst_addr=None
 			dst_port=None
-			if atyp==chr(0x01):#IPV4
+			if atyp=="\x01":#IPV4
 				dst_addr,dst_port=sock.recv(4),sock.recv(2)
 				dst_addr=".".join([str(ord(i)) for i in dst_addr])
-			elif atyp==chr(0x03):#Domain
-				addr_len=ord(sock.recv(4))*256+ord(sock.recv(4))
+			elif atyp=="\x03":#Domain
+				addr_len=ord(sock.recv(1))#域名的长度
 				dst_addr,dst_port=sock.recv(addr_len),sock.recv(2)
 				dst_addr="".join([unichr(ord(i)) for i in dst_addr])
-			elif atyp==chr(0x04):#IPV6
+			elif atyp=="\x04":#IPV6
 				dst_addr,dst_port=sock.recv(16),sock.recv(2)
 				tmp_addr=[]
 				for i in xrange(len(dst_addr)/2):
 					tmp_addr.append(unichr(ord(dst_addr[2*i])*256+ord(dst_addr[2*i+1])))
 				dst_addr=":".join(tmp_addr)
 			dst_port=ord(dst_port[0])*256+ord(dst_port[1])
-			sys.stdout.write("Client wants to connect to %s:%d\n" %(dst_addr,dst_port))
+			getLogger().write("Client wants to connect to %s:%d" %(dst_addr,dst_port))
 			server_sock=sock
 			server_ip="".join([chr(int(i)) for i in ip.split(".")])
 
-			if cmd==chr(0x02):#BIND
+			if cmd=="\x02":#BIND
 				#Unimplement
 				sock.close()
-			elif cmd==chr(0x03):#UDP
+			elif cmd=="\x03":#UDP
 				#Unimplement
 				sock.close()
-			elif cmd==chr(0x001):#CONNECT
-				sock.sendall(VER+SUCCESS+chr(0x00)+chr(0x01)+server_ip+chr(port/256)+chr(port%256))
-				sys.stdout.write("Starting transform thread\n")
+			elif cmd=="\x01":#CONNECT
+				sock.sendall(VER+SUCCESS+"\x00"+"\x01"+server_ip+chr(port/256)+chr(port%256))
+				getLogger().write("Starting transform thread")
 				SocketTransform(server_sock,dst_addr,dst_port).start()
 			else:#Unspport Command
 				sock.sendall(VER+UNSPPORTCMD+server_ip+chr(port/256)+chr(port%256))
 				sock.close()
 		except Exception,e:
-			sys.stderr.write("Error on starting transform:"+e.message+"\n")
+			getLogger().write("Error on starting transform:"+e.message,Log.ERROR)
 			sock.close()
 
 class OnExit:
@@ -139,10 +165,9 @@ class OnExit:
 
 if __name__=='__main__':
 	try:
-		ip="192.168.1.14"
+		ip="0.0.0.0"
 		port=8080
 		create_server(ip,port)
 	except Exception,e:
-		sys.stderr.write("Error on create server:"+e.message+"\n")
-
+		getLogger().write("Error on create server:"+e.message,Log.ERROR)
 
